6fe62148b23f85f3887d8684e486225283f27134
Duco Dokter <dokter@w20e.com>
2013-03-29 16:19:33 +0100

    Added admin stuff

---
+++ djinn_announcements/admin.py
@@ -0,0 +1,16 @@
+from django.contrib import admin
+from models.announcement import Announcement
+from models.announcementupdate import AnnouncementUpdate
+from models.serviceannouncement import ServiceAnnouncement
+
+
+class AnnouncementUpdateInline(admin.TabularInline):
+    model = AnnouncementUpdate
+
+
+class AnnouncementAdmin(admin.ModelAdmin):
+    list_display = ('title', )
+    search_fields = ['title']
+    inlines = [ AnnouncementUpdateInline, ]
+
+admin.site.register(Announcement, AnnouncementAdmin)
