c847c14d7d41a42071bb387a3964bdab65bb030d
Martin Bouma <martin@studioreactor.com>
2021-05-19 00:19:25 +0200

    added owner to index

--- djinn_announcements/search_indexes.py
+++ djinn_announcements/search_indexes.py
@@ -4,6 +4,17 @@ from djinn_announcements.models.serviceannouncement import ServiceAnnouncement
 from pgsearch.base import ContentSearchIndex
 
 
+# class AnnouncementIndex(ContentSearchIndex, indexes.Indexable):
+#
+#     def index_queryset(self, using=None):
+#
+#         return self.get_model()._default_manager.all()
+#
+#     def get_model(self):
+#
+#         return Announcement
+
+
 class ServiceAnnouncementIndex(ContentSearchIndex, indexes.Indexable):
 
     def index_queryset(self, using=None):
